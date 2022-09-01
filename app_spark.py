5c15f1a53423a6eee72203a67e4f43e27cad3fbf
Ramon <ramonbarbosafreitas@gmail.com>
2022-09-01 20:39:49 -0300

    Construção de entrada de dados para escolha da coluna para agrupar os dados e visualização das possiveis escolhas de colunas

--- app_spark.py
+++ app_spark.py
@@ -3,11 +3,20 @@
 import connect_spark
 from connect_spark import session
 from sys import argv
+from sys import stdin
+from sys import stdout
+
 arq = argv[1]
+
 def main(object_name):
 	object_name.open_session()
 	object_name.read_file(f"/Users/ramon/Documents/Dados/{arq}")
+	stdout.write(f"{object_name.cols()}\n")
+	object_name.set_choose()
 	object_name.show(35)
+	col = object_name.choose[:-1]
+	object_name.agrupa_conta(f"{col}")
+	object_name.show_grupo(40)
 
 if(__name__ != "__main__"):
 	pass
--- connect_spark.py
+++ connect_spark.py
@@ -1,10 +1,15 @@
 #!usr/bin/env python3
+# -*- encoding:utf-8 -*-
 
 import pyspark
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import*
 from pyspark.sql.types import*
+from sys import stdin
+from sys import stdout
+
+
 
 class session:
 
@@ -12,6 +17,9 @@ class session:
 		self.__sc = None
 		self.__spark = None
 		self.__dado = None
+		self.__agrupado = None
+		self.__cols = None
+		self.__choose = None
 
 	def open_session(self):
 		self.__sc = SparkContext()
@@ -25,5 +33,25 @@ class session:
 			.csv(path)
 		return self.__dado
 
+	def agrupa_conta(self, name_col):
+		self.__agrupado = self.__dado.groupby(name_col).count()
+		self.__agrupado = self.__agrupado.sort(col(name_col).asc())
+		return self.__agrupado
+
+	def cols(self):
+		self.__cols = self.__dado.columns
+		return self.__cols
+
 	def show(self, limit):
 		return self.__dado.show(limit, truncate=False)
+
+	def show_grupo(self, limit):
+		return self.__agrupado.show(limit, truncate=False)
+
+	def set_choose(self):
+		stdout.write("Escolha a coluna para agrupamento da análise\n")
+		self.__choose = stdin.readline()
+
+	@property
+	def choose(self):
+		return self.__choose
