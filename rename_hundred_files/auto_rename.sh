b28c0c5c942d651224d2b96ad2a24549730caf6e
Ramon <ramonbarbosafreitas@gmail.com>
2022-09-02 20:27:09 -0300

    novo codigo em shell script para auto renomeio de mil arquivos em sequencia

---
+++ rename_hundred_files/auto_rename.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+clear
+
+echo "##################################################################"
+echo "##################################################################"
+echo "############ Auto rename a lot of files in sequence ##############"
+echo "##################################################################"
+echo "##################################################################"
+echo ""
+
+echo "by RamonData"
+echo ""
+
+cd /Users/ramon/Documents/files_series/
+
+wc -l /Users/ramon/Documents/files_series/* > /Users/ramon/Documents/files_series/total_files.txt
+
+
