a1c0da1b0b210605030b191014bad4c5361330f9
Ramon <ramonbarbosafreitas@gmail.com>
2022-09-03 19:58:06 -0300

    novo código de teste do módulo time para pausas no código

---
+++ teste_lib_time.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from time import sleep
+
+print("Hello world...my name is Rocky balboa")
+sleep(10)
+print("Then...lets code a lot!!")
+sleep(5)
+print("it is the end this code")
